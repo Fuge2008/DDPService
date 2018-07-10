3a03cd3ee5e32ea4253b80b9773b90ccd3f05e04
Fuge2008 <fzwooo@163.com>
2018-07-10 15:41:50 +0800

    dinadi public service

---
+++ src/main/java/com/diandi/ddpservice/DdpserviceApplication.java
@@ -0,0 +1,12 @@
+package com.diandi.ddpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdpserviceApplication.class, args);
+	}
+}
